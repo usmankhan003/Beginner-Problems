c811a739e6911cd541ab4973db47868c0a963a65
MUHAMMAD USMAN <143386561+usmankhan003@users.noreply.github.com>
2024-11-16 21:07:22 +0500

    problem 3 solved

---
+++ Problem_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string firstName;
+
+    // Ask for the user's first name
+    cout << "Enter your first name: " << endl;
+    cin >> firstName;
+
+    // Check if the name is Alice or Bob
+    if (firstName == "Alice" || firstName == "Bob") {
+        cout << "Welcome,  " << firstName << "!" << endl;
+    } else {
+        cout << "Sorry! You are not Alice or Bob" << endl;
+    }
+
+    return 0;
+}
