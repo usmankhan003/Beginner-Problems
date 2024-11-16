86c8fe8849de538e4b829a30a0e3f4079b4e65c2
usmankhan003 <usmankhanafridi780@gmail.com>
2024-11-16 16:51:41 +0500

    Take name from user with Greet

--- Problem_2.cpp
+++ Problem_2.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string firstName, lastName;
+
+    // Ask for the user's first name
+    cout << "Enter your first name: " << endl;
+    cin >> firstName;
+
     // Ask for the user's last name
+    cout << "Enter your last name: " << endl;
+    cin >> lastName;
+
+    // Greet the user with their full name
+    cout << "Hello, " << firstName << " " << lastName << "! Welcome!" << endl;
+
+    return 0;
+}
